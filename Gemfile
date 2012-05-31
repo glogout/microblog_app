9b448e4ee7c51a166d6d6c56b2a24d9004bec000
glogout <gloria.gouty@gmail.com>
2012-05-30 21:43:45 -0700

    Initial Commit

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.3'
+
+group :development do
+  gem 'sqlite3', '1.3.6'
+end
+
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '3.2.5'
+  gem 'coffee-rails', '3.2.2'
+
+  gem 'uglifier', '1.2.4'
+end
+
+gem 'jquery-rails', '2.0.2'
+
+group :production do
+  gem 'pg', '0.12.2'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MicroblogApp::Application.config.secret_token = '13380900a412f3727ad77b9277c3e35b392e80a5da247b403c0a62c52ba5708178610bac06278523128c7f04126cfcccf3dd38478b65a21038078c47b4bc59fb'
